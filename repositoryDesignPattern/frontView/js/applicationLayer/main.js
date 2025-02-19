cad6382e898a0023167185f7e82140a79385ec80
KenaiAngel <angel.sanchezroj@alumno.buap.mx>
2025-02-19 14:02:44 -0600

    Add functionality to the applicationLayer

--- repositoryDesignPattern/frontView/js/applicationLayer/main.js
+++ repositoryDesignPattern/frontView/js/applicationLayer/main.js
@@ -2,13 +2,30 @@ import RepositoyRemoteProduct from '../repositoryLayer/repositoryRemoteProduct.j
 import RepositoryCrudProduct from '../repositoryLayer/RepositoryCrudProduct.js';
 import Product from '../models/product.js';
 
-const hola = new RepositoyRemoteProduct();
+const remoteAccessProduct = new RepositoyRemoteProduct();
 
-hola.loadProduct().then(() => {
-    const hola2 = new RepositoryCrudProduct(hola1.getProducts);
-}).catch(message=>{
-    console.error(message);
-});
+const localStorageProducts = localStorage.getItem('products');
+
+if (localStorageProducts === null) {
+    remoteAccessProduct.loadProduct().then((products) => {
+        
+        localStorage.setItem('products', JSON.stringify(products));
+
+        const repoCrudProduct = new RepositoryCrudProduct(products);
+        console.log("HolaRemote",repoCrudProduct.listOfProduct);
+    }).catch(message => {
+        console.error(message);
+    });
+} else {
+    // Si los productos ya están en localStorage, los usamos directamente
+    const products = JSON.parse(localStorageProducts);
+    const repoCrudProduct = new RepositoryCrudProduct(products);
+    repoCrudProduct.orderArray();
+    console.log("HolaLocal",repoCrudProduct.listOfProduct);
+}
+
+
+
 
 
 
